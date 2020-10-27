d76419a396e7e143c1bb0beb4823bff13be1242a
Oliver Torbøl Rasborg Størum <oliv606m@elevcampus.dk>
2020-10-27 14:12:33 +0100

    Main

--- DomainSetup/VMSetup.ps1
+++ DomainSetup/VMSetup.ps1
@@ -6,14 +6,13 @@ function Install-VMRoles {
     
     begin {
 
-        # Client VM Session Initialization
         $pass = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+
+        # Client VM Credentials
         $credClient = New-Object System.Management.Automation.PSCredential (“Egon”, $pass)
-        $s_Klient1,$s_Klient2 = New-PSSession -VMName Klient1,Klient2 -Credential $credClient
         
-        # Server VM Session Initialization
+        # Server VM Credentials
         $credServer = New-Object System.Management.Automation.PSCredential (“Administrator”, $pass)
-        $s_Member, $s_Router, $s_Server1, $s_Server2 = New-PSSession -VMName Member,Router,Server1,Server2 -Credential $credServer
 
         # Domain Credentials
         $credDomain1 = New-Object System.Management.Automation.PSCredential (“Domain1\Administrator”, $pass)
@@ -25,24 +24,27 @@ function Install-VMRoles {
         try {
 
             #region: Server1
-            while ((Invoke-Command -Session $s_Server1 -ScriptBlock{"Test"}) -ne Test) {
+            $vmName = "Server1"
+            while ((Invoke-Command -VMName $vmName -Credential $credServer -ScriptBlock{"Test"} -ErrorAction SilentlyContinue) -ne "Test") {
                 Start-Sleep -Seconds 2
+                Write-Output "$vmName is currently booting"
             }
-            Invoke-Command -Session $s_Server1 -ScriptBlock {
+            Invoke-Command -VMName $vmName -Credential $credServer -ScriptBlock {
                 # IP Configuration
                 New-NetIPAddress -IPAddress 10.0.1.10 -InterfaceAlias "Ethernet" -DefaultGateway 10.0.1.1 -PrefixLength 24 | Out-Null
                 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 127.0.0.1 | Out-Null
                 Disable-NetAdapterBinding -InterfaceAlias "Ethernet" -ComponentID ms_tcpip6 | Out-Null
                 
                 # Rename PC
-                Rename-Computer -NewName "Server1" -Force -Restart
+                Rename-Computer -NewName $vmName -Force -Restart
             }
 
-            while ((Invoke-Command -Session $s_Server1 -ScriptBlock{"Test"}) -ne Test) {
+            while ((Invoke-Command -VMName $vmName -Credential $credServer -ScriptBlock{"Test"} -ErrorAction SilentlyContinue) -ne "Test") {
                 Start-Sleep -Seconds 2
+                Write-Output "$vmName is currently rebooting"
             }
 
-            Invoke-Command -Session $s_Server1 -ScriptBlock {
+            Invoke-Command -VMName $vmName -Credential $credServer -ScriptBlock {
                 $Pass = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
                 Install-windowsfeature AD-domain-services -IncludeManagementTools
                 Import-Module ADDSDeployment
@@ -60,20 +62,28 @@ function Install-VMRoles {
                 -Force:$true
             }
 
+            Invoke-Command -VMName $vmName -Credential $credDomain1 -ScriptBlock {
+                # Adds Conditional Forwarder for domain2
+                Add-DnsServerConditionalForwarderZone -Name Domain2.local -MasterServers 10.0.2.10
+            }
+            $vmName = $null
+
             #endregion
             #region: Klient1
-            while ((Invoke-Command -Session $s_Klient1 -ScriptBlock{"Test"}) -ne "Test") {
+
+            $vmName = "Klient1"
+            while ((Invoke-Command -VMName $vmName -Credential $credClient -ScriptBlock{"Test"} -ErrorAction SilentlyContinue) -ne "Test") {
                 Start-Sleep -Seconds 2
-                Write-Output "Still Rebooting"
+                Write-Output "$vmName is rebooting"
             }
-            Invoke-Command -Session $s_Klient1 -ScriptBlock {
+            Invoke-Command -VMName $vmName -Credential $credClient -ScriptBlock {
                 # IP Configuration
                 New-NetIPAddress -IPAddress 10.0.1.11 -InterfaceAlias "Ethernet" -DefaultGateway 10.0.1.1 -PrefixLength 24 | Out-Null
                 Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 10.0.1.10 | Out-Null
                 Disable-NetAdapterBinding -InterfaceAlias "Ethernet" -ComponentID ms_tcpip6 | Out-Null
                 
                 # Rename and Join PC to Domain
-                Add-Computer -domainname Domain1.local -Credential $credServer -NewName Klient1 -Restart
+                Add-Computer -domainname Domain1.local -Credential $credDomain1 -NewName Klient1 -Restart
             }
             
             #endregion

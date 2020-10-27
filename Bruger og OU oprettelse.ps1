75595854c0f44a2f75d4f1bcd8bf6a89e8bda566
Thomas Louis Merton Hansen <thom856b@elevcampus.dk>
2020-10-27 10:56:49 +0100

    main

--- Bruger og OU oprettelse.ps1
+++ Bruger og OU oprettelse.ps1
@@ -11,14 +11,14 @@ New-ADOrganizationalUnit -Name "IT" -Path "OU=Company,DC=Domain1,DC=local"
 New-ADOrganizationalUnit -Name "Produktion" -Path "OU=Company,DC=Domain1,DC=local"
 
 #Oprettelse af brugere samt simpel info
-New-ADUser -Name "Peter Produktion" -DisplayName "PP" -EmailAddress "PP@Domain1.local" -ChangePasswordAtLogon $true
--Initials "PP" -Path "DC=domain1,DC=local,OU=Company,OU=Produktion" -AccountPassword "Start2020"
+New-ADUser -Name "Peter Produktion" -UserPrincipalName "pp@domain1.local" -DisplayName "PP" -EmailAddress "PP@Domain1.local" -ChangePasswordAtLogon 1 `
+-Initials "PP" -Path "OU=Produktion,OU=Company,DC=domain1,DC=local" -AccountPassword (ConvertTo-SecureString "Start2020" -AsPlainText -Force) -enabled 1
 
 New-ADUser -Name "Iben IT" -DisplayName "II" -EmailAddress "II@Domain1.local" -ChangePasswordAtLogon $true
--Initials "II" -Path "DC=domain1,DC=local,OU=Company,OU=IT" -AccountPassword "Start2020"
+-Initials "II" -Path "OU=IT,OU=Company,DC=domain1,DC=local" -AccountPassword "Start2020"
 
 New-ADUser -Name "Anne Administration" -DisplayName "AA" -EmailAddress "AA@Domain1.local" -ChangePasswordAtLogon $true
--Initials "AA" -Path "DC=domain1,DC=local,OU=Company,OU=Administration" -AccountPassword "Start2020"
+-Initials "AA" -Path "OU=Administration,OU=Company,DC=domain1,DC=local" -AccountPassword "Start2020"
 
 New-ADUser -Name "Kasper Kundeservice" -DisplayName "KK" -EmailAddress "KK@Domain1.local" -ChangePasswordAtLogon $true
--Initials "KK" -Path "DC=domain1,DC=local,OU=Company,OU=Kundeservice" -AccountPassword "Start2020"
+-Initials "KK" -Path "OU=Kundeservice,OU=Company,DC=domain1,DC=local" -AccountPassword "Start2020"

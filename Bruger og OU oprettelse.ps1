c65f1c98597823ddc1c65725e126fc45ebdb7ed3
Jonas Mylord Grønbek <jona9263@elevcampus.dk>
2020-10-27 11:31:38 +0100

    main

--- Bruger og OU oprettelse.ps1
+++ Bruger og OU oprettelse.ps1
@@ -14,11 +14,11 @@ New-ADOrganizationalUnit -Name "Produktion" -Path "OU=Company,DC=Domain1,DC=loca
 New-ADUser -Name "Peter Produktion" -UserPrincipalName "pp@domain1.local" -DisplayName "PP" -EmailAddress "PP@Domain1.local" -ChangePasswordAtLogon 1 `
 -Initials "PP" -Path "OU=Produktion,OU=Company,DC=domain1,DC=local" -AccountPassword (ConvertTo-SecureString "Start2020" -AsPlainText -Force) -enabled 1
 
-New-ADUser -Name "Iben IT" -DisplayName "II" -EmailAddress "II@Domain1.local" -ChangePasswordAtLogon $true
--Initials "II" -Path "OU=IT,OU=Company,DC=domain1,DC=local" -AccountPassword "Start2020"
+New-ADUser -Name "Iben IT" -UserPrincipalName "II@domain1.local" -DisplayName "II" -EmailAddress "II@Domain1.local" -ChangePasswordAtLogon 1 `
+-Initials "II" -Path "OU=IT,OU=Company,DC=domain1,DC=local" -AccountPassword (ConvertTo-SecureString "Start2020" -AsPlainText -Force) -enabled 1
 
-New-ADUser -Name "Anne Administration" -DisplayName "AA" -EmailAddress "AA@Domain1.local" -ChangePasswordAtLogon $true
--Initials "AA" -Path "OU=Administration,OU=Company,DC=domain1,DC=local" -AccountPassword "Start2020"
+New-ADUser -Name "Anne Administration" -UserPrincipalName "AA@domain1.local" -DisplayName "AA" -EmailAddress "AA@Domain1.local" -ChangePasswordAtLogon 1 `
+-Initials "AA" -Path "OU=Administration,OU=Company,DC=domain1,DC=local" -AccountPassword (ConvertTo-SecureString "Start2020" -AsPlainText -Force) -enabled 1
 
-New-ADUser -Name "Kasper Kundeservice" -DisplayName "KK" -EmailAddress "KK@Domain1.local" -ChangePasswordAtLogon $true
--Initials "KK" -Path "OU=Kundeservice,OU=Company,DC=domain1,DC=local" -AccountPassword "Start2020"
+New-ADUser -Name "Kasper Kundeservice" -UserPrincipalName "KK@domain1.local" -DisplayName "KK" -EmailAddress "KK@Domain1.local" -ChangePasswordAtLogon 1 `
+-Initials "KK" -Path "OU=Kundeservice,OU=Company,DC=domain1,DC=local" -AccountPassword (ConvertTo-SecureString "Start2020" -AsPlainText -Force) -enabled 1

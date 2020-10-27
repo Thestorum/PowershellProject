27c898e26408b5e4ad848f436f4d38dc1ac8783a
Oliver Torbøl Rasborg Størum <oliv606m@elevcampus.dk>
2020-10-27 13:52:51 +0100

    main

--- DomainSetup/VMSetup.ps1
+++ DomainSetup/VMSetup.ps1
@@ -15,32 +15,74 @@ function Install-VMRoles {
         $credServer = New-Object System.Management.Automation.PSCredential (“Administrator”, $pass)
         $s_Member, $s_Router, $s_Server1, $s_Server2 = New-PSSession -VMName Member,Router,Server1,Server2 -Credential $credServer
 
+        # Domain Credentials
+        $credDomain1 = New-Object System.Management.Automation.PSCredential (“Domain1\Administrator”, $pass)
+        $credDomain2 = New-Object System.Management.Automation.PSCredential (“Domain2\Administrator”, $pass)
+
     }
     
     process {
-        
+        try {
 
-        #region: Klient 1
+            #region: Server1
+            while ((Invoke-Command -Session $s_Server1 -ScriptBlock{"Test"}) -ne Test) {
+                Start-Sleep -Seconds 2
+            }
+            Invoke-Command -Session $s_Server1 -ScriptBlock {
+                # IP Configuration
+                New-NetIPAddress -IPAddress 10.0.1.10 -InterfaceAlias "Ethernet" -DefaultGateway 10.0.1.1 -PrefixLength 24 | Out-Null
+                Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 127.0.0.1 | Out-Null
+                Disable-NetAdapterBinding -InterfaceAlias "Ethernet" -ComponentID ms_tcpip6 | Out-Null
+                
+                # Rename PC
+                Rename-Computer -NewName "Server1" -Force -Restart
+            }
 
-        
+            while ((Invoke-Command -Session $s_Server1 -ScriptBlock{"Test"}) -ne Test) {
+                Start-Sleep -Seconds 2
+            }
 
-        New-NetIPAddress -IPAddress 10.0.1.11 -InterfaceAlias "Ethernet 2" -DefaultGateway 10.0.1.1 -PrefixLength 24
-        Set-DnsClientServerAddress -InterfaceAlias “Ethernet 2” -ServerAddresses 10.0.1.10
-        Disable-NetAdapterBinding -InterfaceAlias "Ethernet 2" -ComponentID ms_tcpip6
+            Invoke-Command -Session $s_Server1 -ScriptBlock {
+                $Pass = ConvertTo-SecureString "P@ssw0rd" -AsPlainText -Force
+                Install-windowsfeature AD-domain-services -IncludeManagementTools
+                Import-Module ADDSDeployment
+                Install-ADDSForest -SafeModeAdministratorPassword $Pass `
+                -DomainName Domain1.local `
+                -CreateDnsDelegation:$false `
+                -DatabasePath "C:\Windows\NTDS" `
+                -DomainMode Default `
+                -DomainNetbiosName "DOMAIN1" `
+                -ForestMode Default `
+                -InstallDns:$true `
+                -LogPath "C:\Windows\NTDS" `
+                -NoRebootOnCompletion:$false `
+                -SysvolPath "C:\Windows\SYSVOL" `
+                -Force:$true
+            }
 
-        Add-Computer -domainname Domain1.local -Credential $cred -NewName Klient1 -Restart
+            #endregion
+            #region: Klient1
+            while ((Invoke-Command -Session $s_Klient1 -ScriptBlock{"Test"}) -ne "Test") {
+                Start-Sleep -Seconds 2
+                Write-Output "Still Rebooting"
+            }
+            Invoke-Command -Session $s_Klient1 -ScriptBlock {
+                # IP Configuration
+                New-NetIPAddress -IPAddress 10.0.1.11 -InterfaceAlias "Ethernet" -DefaultGateway 10.0.1.1 -PrefixLength 24 | Out-Null
+                Set-DnsClientServerAddress -InterfaceAlias "Ethernet" -ServerAddresses 10.0.1.10 | Out-Null
+                Disable-NetAdapterBinding -InterfaceAlias "Ethernet" -ComponentID ms_tcpip6 | Out-Null
+                
+                # Rename and Join PC to Domain
+                Add-Computer -domainname Domain1.local -Credential $credServer -NewName Klient1 -Restart
+            }
+            
+            #endregion
 
-        #endregion
-
-        #region PS REMOTE
-        Enable-PSRemoting -Force
-        #endregion
-
-
-
-
+        }
+        catch {
+            
+        }
     }
-    
     end {
         
     }

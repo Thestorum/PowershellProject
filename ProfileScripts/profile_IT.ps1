aa1213e8648b0a4bde188ded4d1fd80561e5a77b
Thomas Louis Merton Hansen <thom856b@elevcampus.dk>
2020-10-29 09:55:21 +0100

    main

--- ProfileScripts/profile_IT.ps1
+++ ProfileScripts/profile_IT.ps1
@@ -1,6 +1,3 @@
-#Oprettelse af profile scripts til forskellige brugere
-New-Item -path $profile -type file -Force
-
 #Profile script til Iben IT
 $Shell = $Host.UI.RawUI
 $Shell.WindowTitle="Powershell Iben IT"
